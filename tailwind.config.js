61f602fe619eeb66c23776cc14ecbda40acb3c5d
cheynnn <christianemaybcomia@iskolarngbayan.pup.edu.ph>
2024-05-06 10:36:40 +0800

    added some animations and transitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,15 @@ module.exports = {
         'halant': ['Halant', 'serif'],
         'hammersmith-one': ['Hammersmith One', 'sans-serif'],
       },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   plugins: [],
